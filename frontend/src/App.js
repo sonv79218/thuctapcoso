0f7da7af053847aa3659515d3d7e93a56731d214
son vu <sonv79218@gmail.com>
2024-11-16 11:41:47 +0700

    bảo vệ route. tránh trường hợp nhập đường dẫn trên URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import {
   Routes,
   useLocation,
 } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import Header from "./Header_footer/Header";
 //import ButtonBack from "./Header_footer/ButtonBack";
 import Profile from "./components/Profile.js";
@@ -27,11 +27,26 @@ import Error from "./components/Error.js";
 const AppContent = () => {
   // const userId = sessionStorage.getItem("userId");
   // const role = sessionStorage.getItem("role");
-  const ProtectedRoute = ({ element }) => {
+  const ProtectedRouteAdmin = ({ element }) => {
     const userId = sessionStorage.getItem("userId");
     const role = sessionStorage.getItem("role");
     //const navigate = useNavigate();
     if (userId && role === "admin") {
+      return element;
+    } else {
+      // navigate("/error"); // Nếu không phải admin, trả về element lỗi
+      return <Error />;
+    }
+
+    // nếu userId khác null - là có đăng nhập và role = admin;
+    //if (userId && role === "admin") {
+    // thì đường dẫn /admin-dashboard dẫn đến trang element={<Admin />} còn nếu không thì sẽ dẫn đến
+    // trang element={<Error />}
+  };
+  const ProtectedRoute = ({ element }) => {
+    const userId = sessionStorage.getItem("userId");
+    //const navigate = useNavigate();
+    if (userId) {
       return element;
     } else {
       // navigate("/error"); // Nếu không phải admin, trả về element lỗi
@@ -52,18 +67,34 @@ const AppContent = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/error" element={<Error />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/book/list" element={<BookList />} />
+        <Route
+          path="/book/list"
+          element={<ProtectedRoute element={<BookList />} />}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/upgrade" element={<UpgradeAccount />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/book/add" element={<AddBookForm />} />
-        <Route path="/book" element={<Book />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route
+          path="/upgrade"
+          element={<ProtectedRoute element={<UpgradeAccount />} />}
+        />
+        <Route
+          path="/payment"
+          element={<ProtectedRoute element={<Payment />} />}
+        />
+        <Route
+          path="/book/add"
+          element={<ProtectedRoute element={<AddBookForm />} />}
+        />
+        <Route path="/book" element={<ProtectedRoute element={<Book />} />} />
+        <Route
+          path="/profile"
+          element={<ProtectedRoute element={<Profile />} />}
+        />
+
         {/* Đường dẫn protected cho Admin */}
         <Route
           path="/admin-dashboard"
-          element={<ProtectedRoute element={<Admin />} />}
+          element={<ProtectedRouteAdmin element={<Admin />} />}
         />
         {/* form sửa sách */}
         <Route path="/book/edit/:id" element={<EditBookForm />} />
